ab42c850156b990d7652e2ba906c81aa6afe53c2
sumit <sharmasumit0308@gmail.com>
2020-06-14 12:06:21 +0530

    control forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 //import React from 'react';
 import React, { Component } from 'react';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Menu from './MenuComponents';
 import DistD from './DishDetailComponent';
 import Header from './HeaderComponent';
@@ -56,6 +57,7 @@ class Main extends Component {
                 <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                 <Route path='/menu/:dishId' component={DishWithId} />
                 <Route exact path='/contactus' component={Contact} />
+                <Route path='/aboutus' component={() => <About leaders={this.state.leaders} /> }  />
                 <Redirect to="/home" />
             </Switch>
         <Footer />
